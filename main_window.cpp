b690606e3aa88ecba302dd9ce11653671d701367
niki4smirn <kitos.smirnov@gmail.com>
2022-03-13 18:37:56 +0300

    Add widgets and layout

--- main_window.cpp
+++ main_window.cpp
@@ -1,4 +1,50 @@
 #include "main_window.h"
 
 MainWindow::MainWindow() :
-    QMainWindow(nullptr) {}
+    QMainWindow(nullptr),
+    widget_(new QWidget(this)),
+    layout_(new QGridLayout()) {
+  resize(minimal_size_);
+  setMinimumSize(minimal_size_);
+
+  SetupWidgets();
+
+  setCentralWidget(widget_);
+
+  ConnectWidgets();
+}
+
+void MainWindow::ConnectWidgets() {
+
+}
+
+void MainWindow::SetupWidgets() {
+  actions_list_ = new QListWidget(widget_);
+
+  label_ = new QLabel(widget_);
+  label_->setText("Name: ");
+
+  calendar_ = new QCalendarWidget(widget_);
+  calendar_->setLocale(QLocale::English);
+  calendar_->setFirstDayOfWeek(Qt::DayOfWeek::Monday);
+
+  name_edit_ = new QLineEdit(widget_);
+
+  time_edit_ = new QTimeEdit(widget_);
+
+  add_button_ = new QPushButton(widget_);
+  add_button_->setText("Add");
+
+  layout_->addWidget(actions_list_, 0, 2, 4, 1);
+  layout_->addWidget(label_, 0, 0);
+  layout_->addWidget(name_edit_, 0, 1);
+  layout_->addWidget(calendar_, 1, 0, 1, 2);
+  layout_->addWidget(time_edit_, 2, 0, 1, 2);
+  layout_->addWidget(add_button_, 3, 0, 1, 2);
+
+  layout_->setColumnStretch(0, 1);
+  layout_->setColumnStretch(1, 2);
+  layout_->setColumnStretch(2, 3);
+
+  widget_->setLayout(layout_);
+}
--- main_window.h
+++ main_window.h
@@ -1,8 +1,38 @@
 #pragma once
 
+#include <QCalendarWidget>
+#include <QCheckBox>
+#include <QGridLayout>
+#include <QLabel>
+#include <QLineEdit>
+#include <QListWidget>
 #include <QMainWindow>
+#include <QPushButton>
+#include <QTimeEdit>
 
 class MainWindow : public QMainWindow {
+  Q_OBJECT
+
  public:
   MainWindow();
+
+ private:
+  void ConnectWidgets();
+  void SetupWidgets();
+
+  QWidget* widget_;
+  QGridLayout* layout_;
+
+  QListWidget* actions_list_;
+  QLabel* label_;
+  QCalendarWidget* calendar_;
+  QLineEdit* name_edit_;
+  QTimeEdit* time_edit_;
+  QPushButton* add_button_;
+
+  QDate action_date_;
+  QTime action_time_;
+  QString action_name_;
+
+  QSize minimal_size_{1000, 500};
 };
